cf10efd457a2a719409f38e53bff8bc0f26753ad
mitso2005 <gpetrakis343@gmail.com>
2025-08-08 18:26:43 +1000

    added logo to home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,11 +2,15 @@ import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import heroImage from '../assets/img/hero_image.svg';
 import HomeNav from '../components/HomeNav.jsx';
+import Logo from '../components/Logo.jsx';
 
 export default function Home() {
   return (
     <div>
       <HomeNav />
+      <div className="fade-in delay-100">
+        <Logo />
+      </div>
       <div className="hero-image-container fade-in delay-100">
         <img 
           src={heroImage} 
